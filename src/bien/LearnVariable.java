e9b95a6802dfc56b5ca763c57eaaed8ef4fec1e8
Doreenn92 <85107266+Doreenn92@users.noreply.github.com>
2022-09-07 20:05:59 +0700

    OOP Biến 07/09/2022

---
+++ src/bien/LearnVariable.java
@@ -0,0 +1,11 @@
+package bien;
+
+public class LearnVariable {
+    //biến
+
+
+    public static void main(String[] args) {
+        String strBai1 = "Dinh";
+        System.out.println(strBai1);
+    }
+}
